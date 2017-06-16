d149bf73e17b8fe2318522bef834e5a5c803ec6d
user <user@userde-MacBook-89.local>
2017-06-16 09:33:19 +0800

    ver 0.2

---
+++ pinasdS09/Classes.swift
@@ -0,0 +1,28 @@
+//
+//  Classes.swift
+//  pinasdS09
+//
+//  Created by user on 2017/6/16.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import Foundation
+
+class MyClass01{
+    
+}
+
+class MyClass02{
+    var x:Int
+    init(x:Int){
+        self.x = x
+    }
+}
+
+class MyClass03{
+    var x = 123
+}
+
+class MyClass04{
+    var x:Int?
+}
